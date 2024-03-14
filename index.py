694446a815cdf38a381d341bc20813bc56a50396
enzomtpYT <enzojore@live.fr>
2024-03-14 10:30:02 +0100

    Interations and logging

--- index.py
+++ index.py
@@ -1,68 +1,89 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
-import time, json
+import time, json, re, os
 
 datas = []
 options = FirefoxOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 driver = webdriver.Firefox(options=options)
+print("Firefox Headless Browser Invoked")
 iterations = 0
+doneurls=[]
+t = time.localtime()
+verbose = False
+
+# Create a logger function
+def log(a):
+    """
+    A function that logs a message if verbose is True. 
+    If the log file exists, the message is appended to the file with a timestamp.
+    Parameters:
+        a (str): The message to be logged.
+    """
+    if verbose:
+        log(f"[{time.strftime('%H:%M:%S', t)}] : {a}")
+    if os.path.exists("log.txt"):
+        with open("log.txt", "a") as file:
+            file.write(f"[{time.strftime('%H:%M:%S', t)}] : {a}\n")
+    else:
+        with open("log.txt", "w") as file:
+            file.write(f"[{time.strftime('%H:%M:%S', t)}] : {a}\n")
 
 def get_data(urls, driver, iterations):
-    if iterations > 5:
+    """
+    Get data from a list of URLs using a web driver, and iterate through the list of URLs until all data is retrieved.
+    
+    :param urls: list of URLs to retrieve data from
+    :param driver: web driver to use for retrieving data
+    :param iterations: the current iteration count
+    :return: None
+    """
+    if iterations > 1:
         return
+    print("Iteration: " + str(iterations))
     iterations+=1
-    urls = []
-    print("Firefox Headless Browser Invoked")
+    nurls = []
     for url in urls:
-        print("Getting data from: " + url)
-        data = {
-            "title": "",
-            "year": 0,
-            "rating": 0,
-            "genres": [],
-            "directors": [],
-            "writers": [],
-            "stars": []
-        }
+        if url not in doneurls:
+            doneurls.append(url)
+            log("Getting data from: " + url)
+            data = {"title": "","year": 0,"rating": 0,"genres": []}
 
-        driver.get(url)
+            driver.get(url)
 
-        # Get the title, year, genres of the movie
-        data["title"] = driver.find_element(By.CSS_SELECTOR, ".hero__primary-text").text
-        data["year"] = int(driver.find_element(By.CSS_SELECTOR, "div.sc-69e49b85-0.jqlHBQ > ul > li:nth-child(1) > a").text)
-        data["rating"] = float(driver.find_element(By.CSS_SELECTOR, "div.sc-3a4309f8-0.bjXIAP.sc-69e49b85-1.llNLpA > div > div:nth-child(1) > a > span > div > div.sc-bde20123-0.dLwiNw > div.sc-bde20123-2.cdQqzc > span.sc-bde20123-1.cMEQkK").text.replace(",", "."))
+            # Get the title, year, genres of the movie
+            data["title"] = driver.find_element(By.CSS_SELECTOR, ".hero__primary-text").text
+            data["year"] = int(driver.find_element(By.CSS_SELECTOR, "section > div > div > ul > li:nth-child(1) > a.ipc-link.ipc-link--baseAlt.ipc-link--inherit-color").text)
+            try:
+                data["rating"] = float(driver.find_element(By.CSS_SELECTOR, "span.sc-bde20123-1.cMEQkK").text.replace(",", "."))
+            except:
+                data["rating"] = 0.0
 
-        # Get the  of the movie
-        a = driver.find_elements(By.CSS_SELECTOR , "div.ipc-chip-list__scroller > a > span")
-        for i in a:
-            data["genres"].append(i.text)
+            # Get the  of the movie
+            a = driver.find_elements(By.CSS_SELECTOR , "div.ipc-chip-list__scroller > a > span")
+            for i in a:
+                data["genres"].append(i.text)
+            # Get next urls
+            e = driver.find_elements(By.CSS_SELECTOR, "section:nth-child(20) > div.ipc-shoveler.ipc-shoveler--base.ipc-shoveler--page0 > div.ipc-sub-grid.ipc-sub-grid--page-span-2.ipc-sub-grid--nowrap.ipc-shoveler__grid > div > a")
+            for i in e:
+                url = re.sub(r"\?(.*)", "", i.get_attribute("href"))
+                if url not in doneurls:
+                    log("Adding " + url + " to the list")
+                    nurls.append(url)
+                else:
+                    log(url + " is already in the list")
 
-        # Get the directors of the movie
-        b = driver.find_elements(By.CSS_SELECTOR, "div.sc-69e49b85-3.dIOekc > div > ul > li:nth-child(1) > div > ul > li > a")
-        for i in b:
-            data["directors"].append(i.text)
+            datas.append(data)
+            log("Data from " + url + " has been added to the list")
+            nurls = nurls[0:5]
+            log(nurls)
+            get_data(nurls, driver, iterations)
 
-        # Get the writers of the movie
-        c = driver.find_elements(By.CSS_SELECTOR, "div.sc-69e49b85-3.dIOekc > div > ul > li:nth-child(2) > div > ul > li > a")
-        for i in c:
-            data["writers"].append(i.text)
-
-        # Get the stars of the movie
-        d = driver.find_elements(By.CSS_SELECTOR, "div.sc-69e49b85-3.dIOekc > div > ul > li:nth-child(3) > div > ul > li > a")
-        for i in d:
-            data["stars"].append(i.text)
-
-        datas.append(data)
-        print("Data from " + url + " has been added to the list")
-
-    driver.quit()
-    print("Firefox Headless Browser Closed")
-
-
-get_data(["https://www.imdb.com/title/tt0120737/", "https://www.imdb.com/title/tt1630029/"], driver, iterations)
-print(datas)
+get_data(["https://www.imdb.com/title/tt1630029/", "https://www.imdb.com/title/tt16606592/"], driver, iterations)
+driver.quit()
+log("Firefox Headless Browser Closed")
+log(datas)
 # write datas to a json file
 with open("datas.json", "w", encoding="utf-8") as file:
     json.dump(datas, file, ensure_ascii=False)
