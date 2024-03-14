c03114ab9ada69ce18377384495132e2fad94b1e
enzomtpYT <enzojore@live.fr>
2024-03-14 12:51:04 +0100

    Changed way to think

--- index.py
+++ index.py
@@ -1,17 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 import time, json, re, os
 
 datas = []
-options = FirefoxOptions()
+options = ChromeOptions()
 # options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
-print("Firefox Headless Browser Invoked")
-iterations = 0
-doneurls=[]
+driver = webdriver.ChromiumEdge(options=options)
+print("Chrome Headless Browser Invoked")
 t = time.localtime()
-verbose = False
+verbose = True
+genres = ["Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Drama", "Family", "Fantasy", "Film-Noir", "Game-Show", "History", "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi", "Sport", "Thriller", "War", "Western"]
 
 # Create a logger function
 def log(a):
@@ -22,7 +21,7 @@ def log(a):
         a (str): The message to be logged.
     """
     if verbose:
-        log(f"[{time.strftime('%H:%M:%S', t)}] : {a}")
+        print(f"[{time.strftime('%H:%M:%S', t)}] : {a}")
     if os.path.exists("log.txt"):
         with open("log.txt", "a") as file:
             file.write(f"[{time.strftime('%H:%M:%S', t)}] : {a}\n")
@@ -30,59 +29,36 @@ def log(a):
         with open("log.txt", "w") as file:
             file.write(f"[{time.strftime('%H:%M:%S', t)}] : {a}\n")
 
-def get_data(urls, driver, iterations):
-    """
-    Get data from a list of URLs using a web driver, and iterate through the list of URLs until all data is retrieved.
-    
-    :param urls: list of URLs to retrieve data from
-    :param driver: web driver to use for retrieving data
-    :param iterations: the current iteration count
-    :return: None
-    """
-    if iterations > 1:
-        return
-    print("Iteration: " + str(iterations))
-    iterations+=1
-    nurls = []
-    for url in urls:
-        if url not in doneurls:
-            doneurls.append(url)
-            log("Getting data from: " + url)
-            data = {"title": "","year": 0,"rating": 0,"genres": []}
-
-            driver.get(url)
-
-            # Get the title, year, genres of the movie
-            data["title"] = driver.find_element(By.CSS_SELECTOR, ".hero__primary-text").text
-            data["year"] = int(driver.find_element(By.CSS_SELECTOR, "section > div > div > ul > li:nth-child(1) > a.ipc-link.ipc-link--baseAlt.ipc-link--inherit-color").text)
+def get_data(genres, driver):
+    data = {}
+    for genre in genres:
+        log(f"Getting data for {genre}")
+        driver.get(f"https://www.imdb.com/search/title/?title_type=feature&genres={genre}%2C%21documentary%2C%21short")
+        for i in range(1, 51):
+            name = driver.find_element(By.CSS_SELECTOR, f"li:nth-child({i}) > div > div > div > div.sc-d80c3c78-3.gqHsYK > div.sc-b0691f29-0.jbYPfh > div.ipc-title.ipc-title--base.ipc-title--title.ipc-title-link-no-icon.ipc-title--on-textPrimary.sc-b0691f29-9.klOwFB.dli-title > a > h3").text
+            name = re.sub(r".*.\..", "", name)
             try:
-                data["rating"] = float(driver.find_element(By.CSS_SELECTOR, "span.sc-bde20123-1.cMEQkK").text.replace(",", "."))
+                data[name] = {
+                    "year": driver.find_element(By.CSS_SELECTOR, f"li:nth-child({i}) > div > div > div > div.sc-d80c3c78-3.gqHsYK > div.sc-b0691f29-0.jbYPfh > div.sc-b0691f29-7.hrgukm.dli-title-metadata > span:nth-child(1)").text
+                }
             except:
-                data["rating"] = 0.0
+                data[name]["year"] = {
+                    "year": "0"
+                }
+            try:
+                data[name]["rating"] = driver.find_element(By.CSS_SELECTOR, f"li:nth-child({i}) > div > div > div > div.sc-d80c3c78-3.gqHsYK > div.sc-b0691f29-0.jbYPfh > span > div > span").text
+            except:
+                data[name]["rating"] = "0.0"
+            if "genres" in data[name]:
+                data[name]["genres"].append(genre)
+            else:
+                data[name]["genres"] = [genre]
+            log(f"{name} : {data[name]}")
+    datas.append(data)
 
-            # Get the  of the movie
-            a = driver.find_elements(By.CSS_SELECTOR , "div.ipc-chip-list__scroller > a > span")
-            for i in a:
-                data["genres"].append(i.text)
-            # Get next urls
-            e = driver.find_elements(By.CSS_SELECTOR, "section:nth-child(20) > div.ipc-shoveler.ipc-shoveler--base.ipc-shoveler--page0 > div.ipc-sub-grid.ipc-sub-grid--page-span-2.ipc-sub-grid--nowrap.ipc-shoveler__grid > div > a")
-            for i in e:
-                url = re.sub(r"\?(.*)", "", i.get_attribute("href"))
-                if url not in doneurls:
-                    log("Adding " + url + " to the list")
-                    nurls.append(url)
-                else:
-                    log(url + " is already in the list")
-
-            datas.append(data)
-            log("Data from " + url + " has been added to the list")
-            nurls = nurls[0:5]
-            log(nurls)
-            get_data(nurls, driver, iterations)
-
-get_data(["https://www.imdb.com/title/tt1630029/", "https://www.imdb.com/title/tt16606592/"], driver, iterations)
+get_data(genres, driver)
 driver.quit()
-log("Firefox Headless Browser Closed")
+log("Chrome Headless Browser Closed")
 log(datas)
 # write datas to a json file
 with open("datas.json", "w", encoding="utf-8") as file:
